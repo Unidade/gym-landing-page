518ba16775cda5b351f4fcbfbc8ffb43913f9b0d
Unidade <68622249+Unidade@users.noreply.github.com>
2022-09-12 20:42:33 -0300

    Build for production

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          context: path.resolve(__dirname),
+          from: './src/*.html',
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`
+      new HtmlMinimizerPlugin(),
+    ],
+  },
+}
