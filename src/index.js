59b1299d8d0ab6df66f829d34886a6f9741c7b89
Unidade <68622249+Unidade@users.noreply.github.com>
2022-09-12 20:33:54 -0300

    Finish webpack setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import AOS from 'aos'
+
+function toggleNavbar(collapseID) {
+  document.getElementById(collapseID).classList.toggle('hidden')
+  document.getElementById(collapseID).classList.toggle('block')
+}
+
+AOS.init({
+  delay: 200,
+  duration: 1200,
+  once: false,
+})
